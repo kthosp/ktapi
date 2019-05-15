09e569863f72a3a953838789b9306afa977d20ae
klongthomhospital <klongthomhospital@gmail.com>
2019-05-15 15:14:45 +0700

    add file-system<fs> and Read SQL Script from textfile

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const mysql = require('mysql') //เรียก Mysql
 const app = express() // เอาfunction ใส่ตัวแปร
 const config = require('./config.json') //เรียกไฟล์config.jsonใส่ตัวแปรconfig
 
+const fs = require('fs') //เรียกใช้Object File-system
+require.extensions['.txt']=(module,filename)=>{ //กำหนดRequire Extensions ให้อ่านtxtเป็น utf8
+  module.exports=fs.readFileSync(filename,'utf8')
+}
+let ovstsql =require('./ovstScript.txt') //เรียกไฟล์ใส่ตัวแปร
+//console.log(ovstsql);//แสดงผลในConsole
+
 const PORT = process.env.PORT || 4000//กำหนดพอร์ท
 
 //เชื่อมต่อฐานข้อมูล
@@ -10,11 +17,12 @@ const db=mysql.createConnection(config)
 // console.log(db);
 db.connect() //เชื่อมต่อฐานข้อมูล
 app.get('/ovst',(req,res)=>{ // สร้างRouterไว้เรียกจากBrowser
-  let sql = 'SELECT TABLE_NAME,UPDATE_TIME,TABLE_ROWS '+ //คำสั่งSQL
-  'FROM information_schema.tables '+
-  'WHERE TABLE_SCHEMA = "hos" '+
-  'AND TABLE_NAME = "ovst" '
-  // console.log(sql);
+  // let sql = 'SELECT TABLE_NAME,UPDATE_TIME,TABLE_ROWS '+ //คำสั่งSQL
+  // 'FROM information_schema.tables '+
+  // 'WHERE TABLE_SCHEMA = "hos" '+
+  // 'AND TABLE_NAME = "ovst" '
+  let sql = ovstsql //คำสั่ง sql ที่อ่านจากไฟล์txt
+   console.log(sql);
   let query = db.query(sql,(err,results)=>{ //สั่ง Query
     if(err) throw err //ดักError
     console.log(results) // แสดงผลบนConsole
@@ -28,18 +36,24 @@ app.get('/', (req, res) => {
   res.json({
     message: 'API OK !!!!'
   })
+  console.log('API OK !!!!');
+  
 })
 
 app.get('/status', (req, res) => {
   res.json({
     message: 'API <status> router'
   })
+  console.log('API <status> router');
+  
 })
 
 app.get('/error', (req, res) => {
   res.json({
     message: 'API <Error>'
   })
+  console.log('API <Error>');
+  
 })
 
 //ทดลองเรียกไฟล์Txt
