d46f619fab9a842fc9e6e8620422a1a5945602bc
klongthomhospital <klongthomhospital@gmail.com>
2019-05-15 13:45:15 +0700

    Query from mysql to JSON

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express')
-const app = express()
-const config = require('./config.json')
+const express = require('express') //เรียก Express
+const mysql = require('mysql') //เรียก Mysql
+const app = express() // เอาfunction ใส่ตัวแปร
+const config = require('./config.json') //เรียกไฟล์config.jsonใส่ตัวแปรconfig
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000//กำหนดพอร์ท
 
+//เชื่อมต่อฐานข้อมูล
+const db=mysql.createConnection(config)
+// console.log(db);
+db.connect()
+app.get('/ovst',(req,res)=>{ // สร้างRouterไว้เรียกจากBrowser
+  let sql = 'SELECT TABLE_NAME,UPDATE_TIME,TABLE_ROWS '+ //คำสั่งSQL
+  'FROM information_schema.tables '+
+  'WHERE TABLE_SCHEMA = "hos" '+
+  'AND TABLE_NAME = "ovst" '
+  // console.log(sql);
+  let query = db.query(sql,(err,results)=>{ //สั่ง Query
+    if(err) throw err //ดักError
+    console.log(results) // แสดงผลบนConsole
+    res.json(results)   // แสดงผลบน Browser 
+  })
+  
+})
+
+//ทดลองสร้างRouter
 app.get('/', (req, res) => {
   res.json({
     message: 'API OK !!!!'
@@ -22,9 +42,10 @@ app.get('/error', (req, res) => {
   })
 })
 
-app.get('/config', (req, res) => {
-  res.json(config)
-})
+//ทดลองเรียกไฟล์Txt
+// app.get('/config', (req, res) => {
+//   res.json(config)
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`)
