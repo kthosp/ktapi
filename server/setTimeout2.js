9ee494d39f1e351bf777798a941d8df8c842f9d0
klongthomhospital <klongthomhospital@gmail.com>
2019-06-05 09:10:39 +0700

    remove folder server

--- server/setTimeout2.js
+++
@@ -1,5 +0,0 @@
-function myFunc(arg) { //สร้างfuntion ที่มีการรับค่าตัวแปร arg
-    console.log(`arg was => ${arg}`); //console.log ข้อความ agr was =>  ค่าในตัวแปร arg
-  }
-  
-  setTimeout(myFunc, 2000, 'funky'); // เรียกใช้ setTimeout(เรียกฟังก์ชันที่สร้าง,เวลาที่Delay,กำหนดค่าให้ตัวแปรarg)
